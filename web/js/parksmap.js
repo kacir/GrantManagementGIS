6a48239a7c28cac8a40c75c8c1c9010d63519460
Robert Kacir <kacir@wisc.edu>
2019-06-09 11:09:56 -0500

    Added popups and search control

--- web/js/parksmap.js
+++ web/js/parksmap.js
@@ -2,6 +2,7 @@ console.log("Testing. Does this Work!");
 
 
 var map = L.map('map').setView([ 34.7517595, -92.329416], 8);
+map.zoomControl.setPosition("bottomleft");
 
 var mapboxlink = "https://api.mapbox.com/styles/v1/robertkaciradpt/cjjrecba50sae2snpvqcw8ylq/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicm9iZXJ0a2FjaXJhZHB0IiwiYSI6ImNqZ3BoODQ2NTAwM20ycXJ1OWpkZnh1emkifQ.MBfZdxZljkG8_JeivKerxw";
 var parklessStreetBasemap = L.tileLayer(mapboxlink).addTo(map);
@@ -13,13 +14,24 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
 
 var stateparkslayer = L.esri.dynamicMapLayer({
     url: "http://gis.arkansas.gov/arcgis/rest/services/ADPT/ADPT_MapService_2017/MapServer"
-}).addTo(map);
+});
 var parkPolygon = L.esri.featureLayer({url : "http://gis.arkansas.gov/arcgis/rest/services/ADPT/ADPT_ORGP_scratch/FeatureServer/1",
     where : "type = 'funded park'",
     style : {fillColor : "#003300", stroke : false, fillOpacity : 1}
+}).bindPopup(function(layer){
+    return layer.feature.properties.currentNam;
+}).addTo(map);
 
+var parkIcon = L.icon({
+    iconUrl : "/img/greenpark.png",
+    iconSize : [12,12],
+    iconAnchor : [6,6]
 });
-var grantPoint = L.esri.featureLayer({url : "http://gis.arkansas.gov/arcgis/rest/services/ADPT/ADPT_ORGP_scratch/FeatureServer/0"});
+var grantPoint = L.esri.featureLayer({url : "http://gis.arkansas.gov/arcgis/rest/services/ADPT/ADPT_ORGP_scratch/FeatureServer/0",
+    pointToLayer : function(feature, latlng){
+        return L.marker(latlng, {icon : parkIcon});
+    }
+}).addTo(map);
 var stateProjectBoundary = L.esri.featureLayer({url : "http://gis.arkansas.gov/arcgis/rest/services/ADPT/ADPT_ORGP_MASTER2/MapServer/25",
     where : "type = 'state'",
     style : {fill : false, stroke : true, opacity : 1.0, color : "#FF0000", weight : 4.0}
@@ -42,22 +54,32 @@ var conversionpolygons = L.esri.featureLayer({url : "http://gis.arkansas.gov/arc
         };
     }
 });
+conversionpolygons.bindPopup(function(layer){
+    var conversionDescription = "<h4>" + layer.feature.properties.yearoccur + "-" + layer.feature.properties.label +
+        ": </h4> <br> <p> " + layer.feature.properties.note1 + layer.feature.properties.note1 + "</p>";
+    return conversionDescription;
+});
 
-var houseDistricts = L.esri.featureLayer({url : "http://gis.arkansas.gov/arcgis/rest/services/FEATURESERVICES/Boundaries/FeatureServer/15"});
-var senateDistricts = L.esri.featureLayer({url : "http://gis.arkansas.gov/arcgis/rest/services/FEATURESERVICES/Boundaries/FeatureServer/34"});
+var houseDistricts = L.esri.featureLayer({url : "http://gis.arkansas.gov/arcgis/rest/services/FEATURESERVICES/Boundaries/FeatureServer/15"})
+    .bindTooltip(function(layer){
+        return "District " + layer.feature.properties.ndistrict + ": " + layer.feature.properties.name;
+});
+var senateDistricts = L.esri.featureLayer({url : "http://gis.arkansas.gov/arcgis/rest/services/FEATURESERVICES/Boundaries/FeatureServer/34"})
+    .bindTooltip(function(layer){
+        return "District " + layer.feature.properties.ndistrict + ": " + layer.feature.properties.name;
+});
 var regions = L.esri.featureLayer({url : "http://gis.arkansas.gov/arcgis/rest/services/ADPT/ADPT_ORGP_MASTER2/MapServer/52",
     style : function(feature){
         if (feature.properties.region2017 == "region 1"){
-            return {fillColor : "pink", color : "white"};
+            return {fillColor : "pink", color : "white", fillOpacity : 0.9};
         } else if (feature.properties.region2017 == "region 2"){
-            return {fillColor : "blue" , color : "white"};
+            return {fillColor : "blue" , color : "white", fillOpacity : 0.9};
         } else if (feature.properties.region2017 == "region 3"){
-            return {fillColor : "green" , color : "white"};
+            return {fillColor : "green" , color : "white", fillOpacity : 0.9};
         } else {
             return {fill: false , stroke : false};
         }
     }
-
 });
 
 
@@ -72,6 +94,7 @@ var overlayMaps = {"State Parks" : stateparkslayer ,
     "Senate Districts" : senateDistricts,
     "Project Officier Regions" : regions
 };
-
 var baseMaps = { "Streets" : parklessStreetBasemap, "Aerial" : Esri_WorldImagery};
 L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+L.Control.geocoder({position : "topleft"}).addTo(map);
